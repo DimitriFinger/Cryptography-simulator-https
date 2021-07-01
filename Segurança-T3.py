1c3e5135025134ad697b4bac38c99424338a4a37
DimitriFinger <dimitri.finger@outlook.com>
2021-06-30 22:08:43 -0300

    .

--- Segurança-T3.py
+++ Segurança-T3.py
@@ -31,7 +31,7 @@ from Crypto.Random import get_random_bytes
 #   reversed_plain_text = uelaV .adarfic e aditrevni atlov ed gsm atse adnaM .odut adilavni \xc3\xa1j a\xc3\xa7nerefid ed tib mU .)-: iof aroga euq ohcA
 #   hexadecimal_crypted_message = 12398AF616C99BA20FF21B1F4ED8A333FFA1B0394C365667B0D618710984FA15D7979547CE4B87ABA8B63809050C469CD4EB021C3919DC10259E474D30E5365E8A3FB3175C537C5C8ECDE8052C0C25CC9F64AB14B263BD51CF84272676B2BB5A3373790CE0E426DDE16C5E205AD393CEEF0B1473AEBD951446D4216C0F968E655CFC517AD9D87E490D775B42D656D636
 # 
-# 8: the new received message is decrypted for confirmatition
+# 8: the new message received from teacher is decrypted for confirmatition
 #   crypted_new_message = 5D7304AF32367F61317FF6392DA00AD45146E490E4E57694218E63FC25E01869B8845B390D17C28774971C9FE0390CB29310C45FBAF59B05246239CCDF2DC39F971C508E4BA470DBC3A9D3AA105EB147656DA5CD860AC61BE87FE88FC22338A278D4C2D7FA540032C092745D5382167578EEC9F1BEE2AABAAA961B5621FE2317
 #   new_message = Foi mesmo :-). Agora comenta bem o código e coloca este exemplo completo no início do código. Show.
 #
@@ -53,20 +53,17 @@ def generate_random_a_value(p_decimal):
 # calculate A using random a, A = g_decimal^a mod p_decimal
 def calculate_A(a, p, g):
     A_decimal = pow(g,a,p)
-    print("\nA decimal: ", A_decimal)
     A_hexadecimal = hex(A_decimal)    
     return A_hexadecimal[2:].upper()
 
 # calculate B using B given by teacher and random a (same as before), B = g_decimal^a mod p_decimal
 def calculate_V(B,a,p):
     B_decimal = int(B,16)
-    print("\nB decimal: ", B_decimal)
     return pow(B_decimal,a,p)
 
 # calculate S value and generate hexadecimal password
 def get_password(V):
     V_hexadecimal = hex(V)[2:]
-    print(V_hexadecimal.upper())
     h = SHA256.new()
     h.update(bytes.fromhex(V_hexadecimal))
     S = h.hexdigest().upper()  
@@ -106,7 +103,7 @@ if __name__ == "__main__":
     print("------------------------- First step -----------------------")
 
     a = generate_random_a_value(p_decimal)
-    print("\nrandom a:\n", a)
+    print("\nRandom a:\n", a)
     # a = 123705312412318916704247743587675603001627154551172868392175465183200784471918996863253192551547811473668156778008783691613079446343592061823441420292256291907063602243168905562636317108716473779802217627667873328989749104444477128445677107334922735974961431126411924656090058162704483844784149775442141726090
     
     # A_hexadeximal value sent to teacher
@@ -116,16 +113,11 @@ if __name__ == "__main__":
     #                 CC3FED0B DD60A131 C4744721 BF6B8118 F1F8FAAB 0D6CC603
     #                 0401EAF2 64356C46 5F15625A 41D46449 C08D1CE8 1A0116EA
     #                 B9E40214 2F6790F9    
+
     A_hexadecimal = calculate_A(a,p_decimal,g_decimal)
     print("\nA hexadecimal:\n" + ' '.join([A_hexadecimal[i:i+8] for i in range(0, len(A_hexadecimal), 8)]))
 
     # B_hexadecimal value given by teacher
-    # B_hexadecimal = 7B1DD90F D7313D59 DF4065FD 8F2CB7B2 9A2A40E7 B556014C
-    #                 3821990C 59D3F51E C0C43BE5 65DCA759 D663C202 7C65F88C
-    #                 7D506DF8 6A513C64 36F5ECE2 74DB840A 393CA6C5 D6C66585
-    #                 EEB7C2FF D2E97A12 008437B7 68F28501 32563354 2FC7803F
-    #                 D341B00B FA8B9B91 497D6394 8DA3DACA D356FCE1 E2C998D6
-    #                 D750F9E3 7B031409
     B_hexadecimal = "7B1DD90FD7313D59DF4065FD8F2CB7B29A2A40E7B556014C3821990C59D3F51EC0C43BE565DCA759D663C2027C65F88C7D506DF86A513C6436F5ECE274DB840A393CA6C5D6C66585EEB7C2FFD2E97A12008437B768F28501325633542FC7803FD341B00BFA8B9B91497D63948DA3DACAD356FCE1E2C998D6D750F9E37B031409"
     print("\nB hexadecimal:\n" + ' '.join([B_hexadecimal[i:i+8] for i in range(0, len(B_hexadecimal), 8)]))
 
@@ -133,7 +125,7 @@ if __name__ == "__main__":
     print("\nV value:\n", V)
 
     password = get_password(V)
-    print("\nDiffie-Hellman's password: \n", password)
+    print("\nPassword: \n", password)
 
     print("\n------------------------- Second step -----------------------")
     
@@ -146,7 +138,7 @@ if __name__ == "__main__":
     #           EB498CEE 57E2C211 5A825F76 87A95238 05B644E1 D63EF06B
     message = "A478E62F9A8AE3F83DFBCD619DFA332483FF8E830CC958060947D518BB69EA3763D6FFECF82AA1784DD4EB34941198E8DCEED40F76CD94878F4B2C58AEDA0C8ED0F0E0DFA34F3F71B32FF0C5E2549A9FB21BEB2172FCD97F426755824370D2E3F05918CCC370851BF7206907519205ED3C4DF11AB24BD8E2EB498CEE57E2C2115A825F7687A9523805B644E1D63EF06B"
     #message = '5D7304AF32367F61317FF6392DA00AD45146E490E4E57694218E63FC25E01869B8845B390D17C28774971C9FE0390CB29310C45FBAF59B05246239CCDF2DC39F971C508E4BA470DBC3A9D3AA105EB147656DA5CD860AC61BE87FE88FC22338A278D4C2D7FA540032C092745D5382167578EEC9F1BEE2AABAAA961B5621FE2317'
-    print("\nMessage given by teacher:\n", message)
+    print("\nEncrypted message:\n", message)
 
     # plain_text = Acho que agora foi :-). Um bit de diferença já invalida tudo. Manda esta msg de volta invertida e cifrada. Valeu
     plain_text = AES_decrypt(message, password)
@@ -155,7 +147,7 @@ if __name__ == "__main__":
     # reversed_plain_text = uelaV .adarfic e aditrevni atlov ed gsm atse adnaM .odut adilavni \xc3\xa1j a\xc3\xa7nerefid ed tib mU .)-: iof aroga euq ohcA
     reversed_plain_text = plain_text[::-1].encode()
     print("\nReversed encoded plain text:\n", reversed_plain_text)
-    # message given by teacher
+    # message given to teacher
     # message = 747E1A77 844DCEAD D89F8BFC 78C2B3AA EB821CB4 629FF321
     #           9359B75A 4805C4B9 6DC9F83C DDAE0FAC 44460992 F6EB8228
     #           26B7A538 AA0FBB0D 7939DB9F 3D2D9240 827CBE0A 8241C76C
@@ -164,7 +156,7 @@ if __name__ == "__main__":
     #           5E28F0F4 69939845 CCAA1F6E 54C82AE8 5CE5EC95 3ABA0DE6
 
     reversed_crypted_message = AES_encrypt(reversed_plain_text, password).hex().upper()
-    print("\n Plaint text reversed and encrypted:\n", reversed_crypted_message)
+    print("\n Plain text encrypted:\n", reversed_crypted_message)
     
     
     
